3fb6f9ab0938e19a3f634a3016ae713aae7c6ed1
John Pham <john@djavafactory.com>
2022-07-29 11:11:56 +0700

    Push initial file

---
+++ main.py
@@ -0,0 +1,116 @@
+from env_wrapper import wrap_deepmind
+from model import DQN
+import tianshou as ts
+import torch
+
+def make_atari_env(env_id, frame_stack=4):
+    return wrap_deepmind(env_id=env_id, frame_stack=frame_stack)
+
+
+def test_dqn(env_id,
+             action_space,
+             frame_stack=4,
+             train_num=1,
+             test_num=1,
+             lr=0.0001,
+             gamma=0.99,
+             target_update_freq=500,
+             epochs=1000,
+             step_per_epoch=100000,
+             step_per_collect=10,
+             update_per_step=0.1,
+             batch_size=64,
+             buffer_size=100000,
+             eps_train_start=1,
+             eps_train_min=0.05,
+             eps_test=0.005,
+             n_step=1):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    train_envs = ts.env.ShmemVectorEnv(
+        [lambda: make_atari_env(env_id) for _ in range(train_num)])
+    test_envs = ts.env.ShmemVectorEnv(
+        [lambda: make_atari_env(env_id) for _ in range(test_num)])
+
+    net = DQN(4, 84, 84, action_space, device=device).to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    
+    policy = ts.policy.DQNPolicy(
+        model=net,
+        optim=optimizer,
+        discount_factor=gamma,
+        target_update_freq=target_update_freq,
+        estimation_step=n_step
+    )
+
+    buffer = ts.data.VectorReplayBuffer(
+        total_size=buffer_size,
+        buffer_num=train_num,
+        ignore_obs_next=True,
+        save_only_last_obs=True,
+        stack_num=frame_stack
+    )
+    
+    train_collector = ts.data.Collector(
+        policy=policy,
+        env=train_envs,
+        buffer=buffer,
+        exploration_noise=True
+    )
+    test_collector = ts.data.Collector(
+        policy=policy,
+        env=test_envs,
+        buffer=buffer,
+        exploration_noise=True
+    )
+    
+    logger = ts.utils.WandbLogger(save_interval=1,
+                                  project=env_id,
+                                  name="dqn")
+    
+    
+    def save_fn(policy):
+        torch.save(policy.state_dict(), "policy.pth")
+        
+    def train_fn(epoch, env_step):
+        # nature DQN setting, linear decay in the first 1M steps
+        if env_step <= 1e6:
+            eps = eps_train_start - env_step / 1e6 * (eps_train_start - eps_train_min)
+        else:
+            eps = eps_train_min
+        
+        policy.set_eps(eps)
+        
+        if env_step % 1000 == 0:
+            logger.write("train/env_step", env_step, {"train/eps": eps})
+            
+    def test_fn(epoch, env_step):
+        policy.set_eps(eps_test)
+        
+    def stop_fn(mean_reward):
+        return mean_reward >= 20
+    
+    # test train_collector and start filling replay buffer
+    train_collector.collect(n_step=batch_size * train_num)
+    # trainer
+    result = ts.trainer.offpolicy_trainer(
+        policy=policy,
+        train_collector=train_collector,
+        test_collector=test_collector,
+        max_epoch=epochs,
+        step_per_epoch=step_per_epoch,
+        step_per_collect=step_per_collect,
+        episode_per_test=test_num,
+        batch_size=batch_size,
+        train_fn=train_fn,
+        test_fn=test_fn,
+        stop_fn=stop_fn,
+        save_fn=save_fn,
+        logger=logger,
+        update_per_step=update_per_step,
+        test_in_train=False,
+    )
+    
+    return result
+
+if __name__ == "__main__":
+    test_dqn("snake-gym-10x20-v0", 4)
