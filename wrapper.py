944c023f864b142ec68b6f0f705c478b2e689ecf
John Pham <john@djavafactory.com>
2022-08-14 13:02:32 +0700

    Fix wrapper observation space

--- wrapper.py
+++ wrapper.py
@@ -6,7 +6,7 @@ class NormFrame(gym.ObservationWrapper):
     def __init__(self, env):
         gym.ObservationWrapper.__init__(self, env)
         self.observation_space = gym.spaces.Box(
-            low=0, high=1, shape=(self.height, self.width, 1), dtype=env.observation_space.dtype
+            low=0, high=255, shape=(self.height, self.width, 1), dtype=env.observation_space.dtype
         )
 
     def observation(self, frame):
