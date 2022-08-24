caff254cf04ea832b6f9e74cd30db4294a9d017f
Pham Hoang Minh <minh.pham2000@linecorp.com>
2022-08-24 12:37:35 +0700

    Add CNN structure

--- model.py
+++ model.py
@@ -23,3 +23,37 @@ class MLPNet(torch.nn.Module):
             
         logits = self.model(obs.view(obs.shape[0], -1))
         return logits, state
+    
+
+class CNNNet(torch.nn.Module):
+    def __init__(self, input_height, input_width, input_channels, num_actions, device):
+        super().__init__()
+        self.device = device
+        self.input_height = input_height
+        self.input_width = input_width
+        self.input_channels = input_channels
+        self.num_actions = num_actions
+        self.cnn = torch.nn.Sequential(
+            torch.nn.Conv2d(self.input_channels, 32, kernel_size=8, stride=4, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+        )
+        
+        with torch.no_grad():
+            random_noise = torch.rand(self.input_channels, self.input_height, self.input_width)
+            n_flatten = self.cnn(random_noise).shape[1]
+            
+        self.q_estimate = torch.nn.Sequential(
+            torch.nn.Linear(n_flatten, self.num_actions),
+            torch.nn.ReLU()
+        )
+        
+    def forward(self, obs, state=None, info={}):
+        obs = torch.as_tensor(obs, device=self.device, dtype=torch.float32)
+        obs = self.cnn(obs)
+        obs = self.q_estimate(obs)
+        return obs, state
